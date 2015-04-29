d0a16f2767c03ea339fe25c05d1ca36a165610f6
QMCPL_MAC2 <qmcpl_mac2@QMC239-MAC-AshviniG.local>
2015-04-29 12:40:07 +0530

    @Ashvini :new project for Fetching data in Background

---
+++ coreDataFetchingBackgroud/coreDataFetchingBackgroud/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  coreDataFetchingBackgroud
+//
+//  Created by QMCPL_MAC2 on 29/04/15.
+//  Copyright (c) 2015 qmcpl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
